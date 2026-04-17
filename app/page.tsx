c58a78b9c91f9f4140d915f411b192c177c3d924
Naman-31-nsk <codebreakernaman@gmail.com>
2026-04-17 09:46:03 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,85 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Welcome to My Blog App </h1>
+    <div style={styles.container}>
+      <div style={styles.card}>
+        
+        
+        <h1 style={styles.title}>Naman Singh Kushwaha</h1>
+        <p style={styles.subtitle}>React Developer </p>
 
-      <Link href="/blog">
-        <button>Go to Blog</button>
-      </Link>
+        
+        <p style={styles.desc}>
+          Building modern web apps with Next.js, API integration and clean UI.
+        </p>
 
-      <br /><br />
+        
+        <div style={styles.buttonContainer}>
+          <Link href="/blog">
+            <button style={styles.button}>Go to Blog</button>
+          </Link>
 
-      <Link href="/contact">
-        <button>Contact Page</button>
-      </Link>
+          <Link href="/contact">
+            <button style={styles.buttonSecondary}>Contact Me</button>
+          </Link>
+        </div>
+
+      </div>
     </div>
   );
 }
+
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "80vh",
+    background: "#f0f2f5",
+  },
+  card: {
+    background: "white",
+    padding: "40px",
+    borderRadius: "16px",
+    boxShadow: "0 8px 20px rgba(0,0,0,0.15)",
+    textAlign: "center" as const,
+    width: "350px",
+  },
+  title: {
+    fontSize: "28px",
+    fontWeight: "bold",
+    marginBottom: "5px",
+  },
+  subtitle: {
+    fontSize: "16px",
+    color: "gray",
+    marginBottom: "15px",
+  },
+  desc: {
+    fontSize: "14px",
+    color: "#555",
+    marginBottom: "20px",
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "center",
+    gap: "10px",
+  },
+  button: {
+    padding: "10px 18px",
+    border: "none",
+    borderRadius: "8px",
+    backgroundColor: "#0070f3",
+    color: "white",
+    cursor: "pointer",
+  },
+  buttonSecondary: {
+    padding: "10px 18px",
+    border: "none",
+    borderRadius: "8px",
+    backgroundColor: "#222",
+    color: "white",
+    cursor: "pointer",
+  },
+};
